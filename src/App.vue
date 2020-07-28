27b3a83faee12af4ff1ce36786045950f01d91a8
630020 <damianzolkos@gmail.com>
2020-07-28 10:23:18 +0200

    basic drag and drop functionality

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+<draggable class="category" :animation="200" group="categories">
+        <div
+          v-for="column in columns"
+          :key="column.title"
+          class=""
+        >
+          <p class="categorytitle">{{column.title}}</p>
+          <draggable :list="column.tasks" :animation="200" ghost-class="ghost-card" group="tasks">
+             <div
+              v-for="(task) in column.tasks"
+              :key="task.id"
+              :task="task"
+              class="mt-3 cursor-move"
+              v-bind:class="{ faved: task.isFaved }"
+              >
+            {{task.id}}: {{task.title}}
+            <br>
+            {{task.timestamp}}
+            </div>
+          </draggable>
+        </div>
+</draggable>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import draggable from "vuedraggable";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    draggable
+  }
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+    private columns: Array<object> = [
+        {
+          title: "jeden",
+          tasks: [
+            {
+              id: 1,
+              title: "zrobić rzecz",
+              timestamp: "1595923797",
+              isFaved: false
+            },
+            {
+              id: 2,
+              title: "kupić rzeczy",
+              timestamp: "765676800",
+              isFaved: true
+            },
+            {
+              id: 3,
+              title: "ogarnąć życie",
+              timestamp: "672351872",
+              isFaved: false
+            }
+          ]
+        },
+        {
+          title: "dwa",
+          tasks: [
+            {
+              id: 6,
+              title: "zjeść obiad",
+              timestamp: "765676800",
+              isFaved: true
+            },
+            {
+              id: 7,
+              title: "kupić pierścionek",
+              timestamp: "672351872",
+              isFaved: false
+            }
+          ]
+        }
+    ]
+}
+
 </script>
 
 <style>
@@ -23,7 +88,17 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+.category {
+  margin-top: 20px;
+  padding: 10px;
+}
+.categorytitle {
+  margin-top: 20px;
+  text-decoration: underline
+}
+.faved {
+  color: red;
 }
 </style>
